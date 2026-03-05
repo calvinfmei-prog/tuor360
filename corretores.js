d47db0edaa09cbbb0c6650ff486b82e769cd2fbb
calvinfmei-prog <calvin.fmei@gmail.com>
2026-03-05 15:07:32 -0300

    Add files via upload

---
+++ corretores.js
@@ -0,0 +1,16 @@
+const corretores = [
+  {
+    id: "lucas",
+    codigo: "LUCAS123",
+    nome: "Lucas Almeida",
+    creci: "445566",
+    whatsapp: "5573981157791"
+  },
+  {
+    id: "bruno",
+    codigo: "BRUNO321",
+    nome: "Bruno Aprigio",
+    creci: "33636",
+    whatsapp: "5573981267713"
+  }
+];
