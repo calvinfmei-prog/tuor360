56296c903fd056580e1ef03cd64adbae0458fd55
calvinfmei-prog <calvin.fmei@gmail.com>
2026-03-09 16:30:00 -0300

    Update imovel.js

--- imovel.js
+++ imovel.js
@@ -1,30 +1,15 @@
 const supabaseUrl = "https://zhgfyqkihwyuteexzxgp.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoZ2Z5cWtpaHd5dXRlZXh6eGdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNTI5ODYsImV4cCI6MjA4ODYyODk4Nn0.CvVtLoNM_YRf2pU6wuyeeoLiKTPRDIBuIzQpLZL5e64";
 
-const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
-
-const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
-
-async function carregar(){
-
 const { data, error } = await supabaseClient
 .from("imoveis")
 .select(`
-  *,
-  corretores ( whatsapp ),
-  imovel_extras ( extra )
+*,
+corretores ( whatsapp ),
+imovel_extras ( extra )
 `)
 .eq("id", id)
 .single();
-
-const extrasDiv = document.getElementById("extras");
-
-let listaExtras = data.imovel_extras.map(e => e.extra);
-
-extrasDiv.innerHTML = listaExtras
-.map(extra => `<span class="extra-item">${extra}</span>`)
-.join("");
 
 if(error){
 console.error(error);
@@ -43,6 +28,8 @@ data.area + "m²";
 document.getElementById("whatsapp").href =
 "https://wa.me/" + data.corretores.whatsapp;
 
+/* TOUR */
+
 document.getElementById("tour").innerHTML = `
 <iframe
 width="100%"
@@ -52,6 +39,22 @@ frameborder="0"
 allowfullscreen>
 </iframe>
 `;
+
+/* EXTRAS */
+
+const extrasDiv = document.getElementById("extras");
+
+if(data.imovel_extras && data.imovel_extras.length > 0){
+
+extrasDiv.innerHTML = data.imovel_extras
+.map(e => `<span class="extra-item">${e.extra}</span>`)
+.join("");
+
+}else{
+
+extrasDiv.innerHTML = "Nenhum extra informado.";
+
+}
 
 }
 
