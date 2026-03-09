42be6ab5f3024cb6cdba2e750a1f23ba34c97418
calvinfmei-prog <calvin.fmei@gmail.com>
2026-03-09 16:33:27 -0300

    Update imovel.js

--- imovel.js
+++ imovel.js
@@ -1,6 +1,13 @@
 const supabaseUrl = "https://zhgfyqkihwyuteexzxgp.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoZ2Z5cWtpaHd5dXRlZXh6eGdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNTI5ODYsImV4cCI6MjA4ODYyODk4Nn0.CvVtLoNM_YRf2pU6wuyeeoLiKTPRDIBuIzQpLZL5e64";
 
+const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+async function carregar(){
+
 const { data, error } = await supabaseClient
 .from("imoveis")
 .select(`
