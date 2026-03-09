2f9c24b41fc4b887994a2548e2c153d9a9bdc53f
calvinfmei-prog <calvin.fmei@gmail.com>
2026-03-09 14:18:55 -0300

    Update painel.js

--- painel.js
+++ painel.js
@@ -29,39 +29,58 @@ document.getElementById("titulo").innerText =
 
 async function carregarImoveis(){
 
+await carregarCorretor();
+
 const { data, error } = await supabaseClient
-  .from("imoveis")
-  .select("*")
-  .eq("corretor_id", corretorId);
+.from("imoveis")
+.select("*")
+.eq("corretor_id", corretorId);
 
 const lista = document.getElementById("listaImoveis");
 
+lista.innerHTML = "";
+
 if(error){
-  lista.innerHTML = "Erro ao carregar imóveis.";
-  return;
+lista.innerHTML = "Erro ao carregar imóveis.";
+return;
 }
 
 if(data.length === 0){
-  lista.innerHTML = "Nenhum imóvel cadastrado.";
-  return;
+lista.innerHTML = "Nenhum imóvel cadastrado.";
+return;
 }
 
 data.forEach(imovel => {
 
 lista.innerHTML += `
+
 <div class="card">
 
-<img src="${imovel.imagem}" class="thumb">
+<img src="${imovel.imagem}" class="card-img">
+
+<div class="card-body">
 
 <h3>${imovel.titulo}</h3>
 
-<p>Preço: ${imovel.preco}</p>
+<p>
+${imovel.quartos} quartos •
+${imovel.area}m²
+</p>
 
-<a href="imovel.html?id=${imovel.id}" class="btn">
+<h4>R$ ${imovel.preco}</h4>
+
+<a href="imovel.html?id=${imovel.id}" class="btn-preto">
 Ver Imóvel
 </a>
 
+<a href="https://wa.me/${whatsappCorretor}" class="btn-verde">
+Falar no WhatsApp
+</a>
+
 </div>
+
+</div>
+
 `;
 
 });
