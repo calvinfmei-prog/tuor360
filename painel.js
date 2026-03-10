9be8fcdd07cfd5a3ab67717a23e72ea36f57afb4
calvinfmei-prog <calvin.fmei@gmail.com>
2026-03-10 18:20:22 -0300

    Update painel.js

--- painel.js
+++ painel.js
@@ -1,3 +1,6 @@
+const params = new URLSearchParams(window.location.search);
+const slugCorretor = params.get("corretor");
+
 const supabaseUrl = "https://zhgfyqkihwyuteexzxgp.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoZ2Z5cWtpaHd5dXRlZXh6eGdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNTI5ODYsImV4cCI6MjA4ODYyODk4Nn0.CvVtLoNM_YRf2pU6wuyeeoLiKTPRDIBuIzQpLZL5e64";
 
